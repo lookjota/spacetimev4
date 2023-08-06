a3d70ea65475c6a2550d0d605046f1c13fe06085
krasny <lookjota@gmail.com>
2023-08-06 17:07:40 -0300

    Rota de upload de arquivos / Servindo arquivos estaticos - check

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,22 @@ import 'dotenv/config'
 
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
 
 import jwt from '@fastify/jwt'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -19,6 +29,7 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
